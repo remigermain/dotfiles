ce64675cabf4e343dffbb7eaf37d71bdaf0301ae
remi <remi@localhost.localdomain>
2020-06-28 01:19:01 +0200

    update(vscode): sym link settings vscode

--- vscode/install.sh
+++ vscode/install.sh
@@ -3,8 +3,12 @@
 # echo "Install vscodium..."
 # flatpak install -y flathub com.vscodium.codium
 
+pwd=$(pwd)
 
+echo "link settings vscode..."
 mkdir -p /home/$USER/.config/VSCodium/
-cp -r ./settings/* /home/$USER/.config/VSCodium/
+rm -rf /home/$USER/.config/VSCodium/User/
+ln -s $pwd/settings/User /home/$USER/.config/VSCodium/
 
+echo "install extensions..."
 $(cat extensions.txt | xargs -L 1 echo $(which codium) --install-extension)
